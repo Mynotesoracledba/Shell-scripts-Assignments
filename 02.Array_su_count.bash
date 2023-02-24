b764f3a482be6a0ee2fe19d32392d93525c2d153
MynotesoracleDBA <raghuldrag@gmail.com>
2023-02-24 05:56:49 +0530

    Update 02.Array_su_count.bash
    Updates with explanation

--- 02.Array_su_count.bash
+++ 02.Array_su_count.bash
@@ -1,29 +1,20 @@
-#!/bin/bash
 
-echo "Enter the numbers with space"
-
-read array_value
-total_value=0;
-
-for i in ${array_value[@]}; do
-  let total_value=$total_value+$i
-  
-done
-echo $total_value
-ubuntu $ vi array_count.sh
-ubuntu $ cat array_count.sh 
 #!/bin/bash
 
 #getting  values from user input
 echo "Enter the numbers with space"
-#assinging values in array
+
+#to store the values in array
 read array_value
-#assign a variable value
+
+#assign a variable
 total_value=0;
+
 #loop to read array values
 for i in ${array_value[@]}; do
   let total_value=$total_value+$i
   
 done
+
 #echo sum value of array
 echo $total_value
